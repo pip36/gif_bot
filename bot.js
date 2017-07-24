b3f77006fef0006a19a11d22141acd86dbe117e8
pip36 <phil_23@live.co.uk>
2017-07-24 17:54:59 +0100

    fix env variables

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var Twitter = require('twitter');
 var request = require('request');
 var client = new Twitter({
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_SECRET,
-  access_token_key: ACCESS_TOKEN_KEY,
-  access_token_secret: ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 var tweet = function(){
@@ -15,7 +15,7 @@ var tweet = function(){
   var rand_descriptor_i = Math.floor(Math.random()*descriptor.length);
   var tweet = descriptor[rand_descriptor_i] + " " + animals[rand_animal_i]
 
-  request('http://api.giphy.com/v1/gifs/translate?s=' + tweet + '&api_key='+GIPHY_API_KEY+'&limit=5', function (error, response, body) {
+  request('http://api.giphy.com/v1/gifs/translate?s=' + tweet + '&api_key='+process.env.GIPHY_API_KEY+'&limit=5', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
         gif = info["data"]["bitly_gif_url"]
