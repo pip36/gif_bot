dd5bee64eb4dd71796811543838f0f47c6db333b
pip36 <phil_23@live.co.uk>
2017-07-24 22:15:36 +0100

    fix server response

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.send('it is running\n');
+  res.end('running');
 }).listen(process.env.PORT || 5000);
 
 var Twitter = require('twitter');
